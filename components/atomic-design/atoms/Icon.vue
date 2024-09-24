60973ace3d3ea90da77d2a0103409b297cdf4eda
Andres Arcila <andresarcilaa2@me.com>
2024-09-24 00:25:26 -0500

    own theme colors for daisy ui

---
+++ components/atomic-design/atoms/Icon.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  icon: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <i :class="icon"></i>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
     require('daisyui'),
   ],
   daisyui: {
-    themes: ["light", "dark"],
+    themes: [
+      {
+        myTheme: {
+          "primary": "#41B883",
+          "secondary": "#34495E",
+          "accent": "#35495E",
+          "neutral": "#333333",
+          "base-100": "#333333",
+          "info": "#3498DB",
+          "success": "#2ECC71",
+          "warning": "#F39C12",
+          "error": "#E74C3C",
+        },
+      }
+    ],
   },
 }
