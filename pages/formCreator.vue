b0f80d67467a5a21b8656b19e5b1f6bda6178a84
Andres Arcila <andresarcilaa2@me.com>
2024-10-08 17:07:53 -0500

    show form creator functions

--- pages/formCreator.vue
+++ pages/formCreator.vue
@@ -6,7 +6,7 @@
         <SharedAtomsBodyText>Edit the json and see how changes</SharedAtomsBodyText>
         <SharedAtomsButton @click="mountBasicForm()">Basic form</SharedAtomsButton>
         <SharedAtomsButton @click="mountComplicatedForm()">More complicated</SharedAtomsButton>
-        <SharedMoleculesFormsFormCreator :form-config="formConfig" @change="formChange" :value="testForm">
+        <SharedMoleculesFormsFormCreator :key="formKey" :form-config="formConfig" @change="formChange" :value="testForm">
         </SharedMoleculesFormsFormCreator>
       </div>
       <div class="w-1/2">
@@ -28,6 +28,7 @@ const formConfig: (FormGroup | FormField)[] = [
     validation: 'required'
   }
 ]
+const formKey = ref(0)
 const complicatedForm = '[{ "type": "group", "name": "usuario", "fields": [{ "type": "text", "name": "nombre", "id": "nombre", "validation": "required | minLength:3", "label": "Nombre" }, { "type": "text", "name": "correo", "id": "correo", "validation": "required | email", "label": "Correo Electrónico" }, { "type": "text", "name": "edad", "id": "edad", "validation": "required | number | between:18,100", "label": "Edad" }, { "type": "text", "name": "direccion", "id": "direccion", "validation": "required", "label": "Dirección" }, { "type": "text", "name": "ciudad", "id": "ciudad", "validation": "required", "label": "Ciudad" }, { "type": "text", "name": "pais", "id": "pais", "validation": "required", "label": "País" }, { "type": "password", "name": "contrasena", "id": "contrasena", "validation": "required | minLength:6", "label": "Contraseña" }, { "type": "select", "name": "genero", "id": "genero", "validation": "required", "label": "Género", "options": { "masculino": "Masculino", "femenino": "Femenino", "otro": "Otro" } }, { "type": "textarea", "name": "biografia", "id": "biografia", "validation": "required | minLength:10", "label": "Biografía" }] }, { "type": "title", "label": "Preferencias" }, { "type": "group", "name": "preferencias", "dFlex": true, "fields": [{ "type": "text", "name": "tema", "id": "tema", "validation": "required", "label": "Tema" }, { "type": "text", "name": "notificaciones", "id": "notificaciones", "validation": "required", "label": "Notificaciones" }] }, { "type": "text", "name": "test", "id": "test", "validation": "required", "label": "Test" }]'
 const simpleForm = '[{ "type": "text", "name": "nombre", "id": "nombre", "validation": "required | minLength:3", "label": "Nombre" }, { "type": "text", "name": "correo", "id": "correo", "validation": "required | email", "label": "Correo Electrónico" }]'
 const newForm = ref(JSON.parse(simpleForm))
@@ -38,11 +39,13 @@ const testForm = ref({
 const mountComplicatedForm = () => {
   newForm.value = JSON.parse(complicatedForm);
   testForm.value =  {json:complicatedForm};
+  formKey.value += 1
 }
 
 const mountBasicForm = () => {
   newForm.value = JSON.parse(simpleForm);
   testForm.value = {json:simpleForm};
+  formKey.value += 1
 }
 
 const formChange = (event: any) => {
@@ -50,6 +53,7 @@ const formChange = (event: any) => {
   newForm.value = event.target.value ? JSON.parse(event.target.value) : [];
   testForm.value = event.target.value ? {json:event.target.value} : {json:{}};
   console.log(newForm.value);
+  formKey.value += 1
 }
 </script>
 
