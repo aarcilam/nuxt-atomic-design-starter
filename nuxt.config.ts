dd43d2ed212d1e3975bfefb71a888878d47f295f
Andres Arcila <andresarcilaa2@me.com>
2024-10-11 19:01:06 -0500

    text universa app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  routeRules: {
+    '/': { prerender: true },
+    '/components': { prerender: true },
+    '/edit-blocks': { ssr: false },
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: [
