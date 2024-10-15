62531822bcc6bccc3e1c0f2d229ab4c7eccbcb9c
Andres Arcila <andresarcila2@me.com>
2024-10-15 11:39:28 -0500

    fix home

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,10 @@
 definePageMeta({
   viewTransition: false
 })
-const log = (e)=>{
-  console.log(e);
-}
 </script>
 
 <template>
   <NuxtLayout name="basic">
-    <ExampleForm submit-label="hi" @submit="log" :value="{email: 'juan@ejemplo.com'}"></ExampleForm>
     <BlocksDisplayBlocks :blocks="homeBlocks()"></BlocksDisplayBlocks>
   </NuxtLayout>
 </template>
