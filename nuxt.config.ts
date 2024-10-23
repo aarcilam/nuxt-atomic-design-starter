6d1ac43ad902a3a6e707d1dfbe85895cace90004
Andres Arcila <andresarcila2@me.com>
2024-10-23 08:47:51 -0500

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  routeRules: {
-    '/design-system': { prerender: true },
-  },
+  // routeRules: {
+  //   '/': { prerender: true },
+  //   '/design-system': { prerender: true },
+  //   '/edit-blocks': { ssr: false },
+  // },
   app: {
     pageTransition: { name: "page" },
   },
