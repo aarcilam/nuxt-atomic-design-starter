f3ec9e066f474157e8d5f9ca76e4c76b089b120c
Andres Arcila <andresarcila2@me.com>
2024-11-01 17:21:03 -0500

    theme sleector , lang and user dropdown

---
+++ components/shared/molecules/LangSelector.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+    const { setLocale, locale } = useI18n()
+</script>
+
+<template>
+    <div class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+            <div class="indicator">
+                {{ locale }}
+            </div>
+        </div>
+        <div tabindex="0" class="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
+            <div class="card-body">
+                <span class="text-lg font-bold">{{ $t('changeLang') }}</span>
+                <div class="card-actions">
+                    <SharedAtomsButton @click="setLocale('en')">English</SharedAtomsButton>
+                    <SharedAtomsButton @click="setLocale('es')">Español</SharedAtomsButton>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+  
