74fd990117749e1fc8f2b80cad5c29960e21247d
Andres Arcila <andresarcilaa2@me.com>
2024-11-01 21:43:59 -0500

    navigator mockup

---
+++ components/shared/molecules/NavigatorMockup.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mockup-browser bg-base-300 border">
+    <div class="mockup-browser-toolbar">
+      <div class="input">{{url}}</div>
+    </div>
+    <div class="bg-base-200 flex justify-center px-4 py-16">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  url: String
+})
+</script>
+
+<style>
+
+</style>
